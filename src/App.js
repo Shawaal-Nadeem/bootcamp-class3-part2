0fa96655fb8d83eab5b8501477955312757fc9c1
Shawaal-Nadeem <hafizshawalnadeem@gmail.com>
2022-09-16 10:32:18 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Component from './component';
 
-function App() {
+
+export default function App() {
+  let[count,setCount]=useState(0)
+  let[time,setTime]=useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={time?'daylight':'nightlight'}>
+      <h1 className='heading'>Good {time?'Morning':'Night'}</h1>
+<div className='val'><Component num={count}/></div>
+<div className='btn'>
+<button className='inc' onClick={()=>
+setCount(++count)}>
+Increase
+</button>
+<button className='res' onClick={()=>
+setCount(count=0)}>
+Reset
+</button>
+<button className='dec' onClick={()=>
+setCount(--count)}>
+Decrease
+</button>
+<div>
+<button className='light' onClick={()=>
+setTime(!time)}>Update Day Time</button>
+</div>
+</div>
     </div>
+    
+    
   );
 }
 
-export default App;
---
+++ src/component.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function component(props){
+    return(
+<h3>Value is : {props.num}</h3>
+    )
+}
